2a6e4a6f48bcedf37bf5a05b97f09f3b1891f717
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-25 05:49:27 -0400

    Authentication Routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/auth/registration",  to "auth#registration"
+  post "/auth/login",         to "auth#login"
+  post "/auth/logout",        to "auth#logout"
+
+  get "/auth/auto_login",     to "auth#auto_login"
+  get "auth/user_is_authed",  to "auth#user_is_authed"
 end

3e20d7177b56cf2398dd193a3420322a4cc83ba0
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-21 00:23:51 -0400

    Post hotels - method for creating a hotel

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,12 @@
 class HotelsController < ApplicationController
+
+  def create
+    @hotel = Hotel.create!(hotel_params)
+    json_params(@hotel, :created)
+  end
+
+  def hotel_params
+    params.permit(:name, :phone, :address, :infos, :latlng)
+  end
+
 end

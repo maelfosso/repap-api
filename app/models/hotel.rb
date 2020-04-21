35fecae2c36efcdd2f49a1c75fbff1ec510ad9a1
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-20 23:49:43 -0400

    Relationship between Hotel and User defined

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,3 @@
 class Hotel < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_104407) do
+ActiveRecord::Schema.define(version: 2020_04_21_034729) do
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.string "infos"
+    t.string "latlng"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"

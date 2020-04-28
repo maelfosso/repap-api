0e2a24f05ec1b4892f13b101ebb0d71f34552dbd
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-27 20:57:23 -0400

    Update Hotel. Add photos attributes/relationship

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,8 @@
 class Hotel < ApplicationRecord
   belongs_to :user
 
+  has_many_attached :photos
+
   validates :name, presence: true
   validates :phone, presence: true 
   validates :price, presence: true

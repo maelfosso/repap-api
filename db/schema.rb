d49ac4767b73395ecfb7f01cdf69c7939f528b58
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-05-08 00:23:13 -0400

    Update index in Favorite

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_05_140434) do
     t.integer "hotel_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"user\", \"hotel\"", name: "index_favorites_on_user_and_hotel", unique: true
+    t.index ["user_id", "hotel_id"], name: "index_favorites_on_user_and_hotel", unique: true
     t.index ["hotel_id"], name: "index_favorites_on_hotel_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end

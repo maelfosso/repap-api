356c4cdeb7099494f889a57175edd3033418284b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-05-09 12:08:21 -0400

    remove database cleaner from db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'database_cleaner'
 
-DatabaseCleaner.clean_with(:truncation)
 API_KEY = 'AIzaSyBHaVnhN3gSB5otSpmKqutxuMaL878KItY'
 
 alice = User.create!(
